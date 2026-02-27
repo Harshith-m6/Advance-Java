3bbe223175f09d2c9daf4043cd4adb12f8914d3c
Harshith Kumar M <harshithkumarm6@gmail.com>
2026-02-27 15:06:47 +0530

    Sevlet program Student Details

---
+++ src/main/java/com/rcb/EmployeeDetails/Employee.java
@@ -0,0 +1,45 @@
+package com.rcb.EmployeeDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private long phone;
+	private String work;
+	
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getWork() {
+		return work;
+	}
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+
+}
